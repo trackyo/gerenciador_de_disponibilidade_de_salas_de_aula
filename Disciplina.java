9c4f48b78af51bc4e7e75b2385194ca179b6588e
Matheus Sousa <54778783+trackyo@users.noreply.github.com>
2019-12-07 23:03:18 -0300

    Atualizacao da classe disciplina
    Atualializei a classe disciplina com o cadastro completo dela, alem de uma interface simples para o usuario em caso de consulta de informacoes.
    
    Essa atualizacao nao contem a parte do codigo de leitura de arquivos, ja que ainda estamos discutindo essa questao.

--- Disciplina.java
+++ Disciplina.java
@@ -1,26 +1,26 @@
+package Entities;
 
-package entities;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Disciplina 
 {
     private String nome;
     private long codigo;
-    private ArrayList <Turma> turmas = new ArrayList<Turma>();
-    private String grau;
-    
+    private String abordagem_pedagogica;
+    private ArrayList <Turma> turmas = new ArrayList<>();
+    private String nivel;
+
+    //metodo construtor
+    public Disciplina() {
+        this.setNome("NAO DEFINIDO");
+        this.setAbordagem_pedagogica("ABORDAGEM PEDAGOGICA NAO DEFINIDA");
+        this.setCodigo(0);
+        this.setNivel("NIVEL ACADEMICO NAO DEFINIDO");
+    }       
+    //getters e setters
     public ArrayList<Turma> getTurmas() 
     {
-        return turmas;
+        return this.turmas;
     }
-
     public void setTurmas(ArrayList<Turma> turmas) 
     {
         this.turmas = turmas;
@@ -28,7 +28,7 @@ public class Disciplina
 
     public String getNome() 
     {
-        return nome;
+        return this.nome;
     }
 
     public void setNome(String nome) 
@@ -38,72 +38,82 @@ public class Disciplina
 
     public long getCodigo() 
     {
-        return codigo;
+        return this.codigo;
     }
 
     public void setCodigo(long codigo) 
     {
         this.codigo = codigo;
     }
-
     
-    public void setGrau(String gr) {
-    	this.grau = gr;	
+    public String getAbordagem_pedagogica() 
+    {
+        return this.abordagem_pedagogica;
+    }
+    
+    public void setAbordagem_pedagogica(String abordagem_pedagogica) 
+    {
+        this.abordagem_pedagogica = abordagem_pedagogica;
+    }
+    
+    public void setNivel(String nv) {
+    	this.nivel = nv;	
     } 
-    public String getGrau() {
-    	return this.grau;
+    public String getNivel() {
+    	return this.nivel;
     }
-    public void escolherGrau(String gr) {
-    	System.out.println("Defina o Grau acadêmico da disciplina");
-    	  	if(gr.equalsIgnoreCase("graduação")|| gr.equalsIgnoreCase("graduacao")) {
-    		setGrau("Graduação.");
-    		
-    	}
-    	  	if (gr.equalsIgnoreCase("extensão") || gr.equalsIgnoreCase("extensao")) {
-    			setGrau("Extensão.");
-    	  	
-    	  	}
-    	  	
-    	  	if (gr.equalsIgnoreCase("Lato Sensu")) {
-    	  		setGrau("Lato Sensu.");
-    }
-    	  	if(gr.equalsIgnoreCase("Stricto Sensu")) {
-        		setGrau("Stricto Sensu.");
-        		
-        	}
     
-    	  	else {
-    	  		System.out.println("Erro! Digite um Grau válido!");
-    	  	}
-    	  	System.out.println("O Grau da disciplina é " + getGrau());
+    //metodos personalizados
+    public void informacoesDisciplina(){
+        System.out.println("--------------------------------");
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Codigo: " + this.getCodigo());
+        System.out.println("Abordagem Pedagogica: " + this.getAbordagem_pedagogica());
+    }
+    public void cadastrarNivel(String nv) {
+    	System.out.println("Defina o nivel acadêmico da disciplina");
+        if(nv.equalsIgnoreCase("graduação")|| nv.equalsIgnoreCase("graduacao")){
+            this.setNivel("Graduação.");
+    		
+                }
+    	if (nv.equalsIgnoreCase("extensão") || nv.equalsIgnoreCase("extensao")){
+            this.setNivel("Extensão.");	
+    	    }
+    	  	
+    	if (nv.equalsIgnoreCase("Lato Sensu")) {
+            this.setNivel("Lato Sensu.");
+    }
+    	if(nv.equalsIgnoreCase("stricto sensu")) {
+            this.setNivel("Stricto Sensu.");		
+            }
+    
+    	else{
+            System.out.println("Erro! Digite um Nível válido!");
+            }
+    	System.out.println("Disciplina cadastrada em: " + this.getNivel());
     	      
     }
     
-    public void criaDiretDisciplina()
-    {
-        File Insti = new File("diretorio//disc", this.nome);
-        boolean jk = Insti.mkdir();
+    public void cadastrarNome(String name){
+        this.setNome(name);
     }
     
-    public String salvarLis()//cria um arquivo .txt com o nome de todos os objetos para facilitar a busca em pastas
-    {
-            
-        try 
-        {
-            FileWriter lt = new FileWriter("diretorio//disc//lista.txt",true);
-            PrintWriter plt = new PrintWriter(lt);
-            plt.println(this.nome);
-
-            plt.flush();
-            plt.close();//fecha a escrita
-            lt.close();//fecha o arquivo
-        }
-            
-        catch (IOException ex) 
-        {
-            Logger.getLogger(Disciplina.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return "Lista salva";
+    public void cadastrarCodigo(long code){
+        this.setCodigo(code);
     }
+    
+    public void cadastrarAbordagem(String abordagem){
+        this.setAbordagem_pedagogica(abordagem);
+    }        
+    public void cadastrarDisciplina(String name, long code, String abordagem, String level){
+        this.cadastrarNome(name);
+        this.cadastrarCodigo(code);
+        this.cadastrarAbordagem(abordagem);
+        this.cadastrarNivel(level);
+        System.out.println("Sua disciplina foi cadastrada com as seguintes informacoes: ");
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Codigo: " + this.getCodigo());
+        System.out.println("Abordagem Pedagogica: " + this.getAbordagem_pedagogica());
+        System.out.println("Nivel Academico: " + this.getNivel());
+    }  
 }
